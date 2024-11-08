17639617a99bc7fa42e8c278bb0a565349cfc59e
Mathieu Réquillart <mathieu.requillart@gmail.com>
2024-11-08 18:59:10 +0100

    tries to add musicbrainz toc calcul

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -62,14 +63,6 @@ func fetchJSON(url string, target interface{}) error {
 	return json.NewDecoder(resp.Body).Decode(target)
 }
 
-func getMusicBrainzDiscID() (string, error) {
-	return runCommand("cd-discid", "--musicbrainz")
-}
-
-func getDiscID() (string, error) {
-	return runCommand("cd-discid")
-}
-
 func makeGnuRequest(client *http.Client, url, userAgent string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -80,6 +73,54 @@ func makeGnuRequest(client *http.Client, url, userAgent string) (*http.Response,
 	return client.Do(req)
 }
 
+func parseDiscID(discID string) ([]int, int, error) {
+	fields := strings.Fields(string(discID))
+	if len(fields) < 3 {
+		return nil, 0, fmt.Errorf("unexpected cd-discid output")
+	}
+
+	// Extract start sectors
+	startSectors := make([]int, len(fields)-3)
+	for i := 2; i < len(fields)-1; i++ {
+		sector, err := strconv.Atoi(fields[i])
+		if err != nil {
+			return nil, 0, err
+		}
+		startSectors[i-2] = sector
+	}
+
+	// Extract last track length
+	lastTrackLength, err := strconv.Atoi(fields[len(fields)-1])
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return startSectors, lastTrackLength, nil
+}
+
+func getMusicBrainzDiscID(discID string) (string, error) {
+	startSectors, lastTrackLength, err := parseDiscID(discID)
+	if err != nil {
+		return "", err
+	}
+	numTracks := len(startSectors)
+	totalSectors := startSectors[numTracks-1] + lastTrackLength
+
+	var mbToc strings.Builder
+	mbToc.WriteString(fmt.Sprintf("1 %d %d", numTracks, totalSectors))
+	for _, sector := range startSectors {
+		mbToc.WriteString(fmt.Sprintf(" %d", sector))
+	}
+	return mbToc.String(), nil
+}
+
+func getDiscID() (string, error) {
+	return runCommand("cd-discid")
+}
+
+func getMusicBrainzDiscIDFromCmd() (string, error) {
+	return runCommand("cd-discid", "--musicbrainz")
+}
 
 func fetchGNUDBDiscInfo(discID string) (*DiscInfo, error) {
 	client := &http.Client{}
@@ -238,7 +279,20 @@ func main() {
 	if err != nil {
 		log.Fatalf("error retrieving disc ID: %v", err)
 	}
+	log.Printf(gnuToc)
 	discID := strings.Fields(gnuToc)[0]
+//	mbToc, err := getMusicBrainzDiscID(gnuToc)
+//	log.Printf(mbToc)
+	mbToc, err := getMusicBrainzDiscIDFromCmd()
+	if err != nil {
+		log.Fatalf("error retrieving disc ID: %v", err)
+	}
+	log.Printf(mbToc)
+	mbToc, err := getMusicBrainzDiscID(gnuToc)
+	if err != nil {
+		log.Fatalf("error retrieving disc ID: %v", err)
+	}
+
 	cueFilePath := cachePlaylistPath(discID)
 	if _, err := os.Stat(cueFilePath); err == nil {
 		saveEnvFile(cueFilePath)
@@ -253,10 +307,6 @@ func main() {
 	if err != nil {
 		log.Printf("info: GNUDB retrieval failed, trying MusicBrainz. Error: %v", err)
 
-		mbToc, err := getMusicBrainzDiscID()
-		if err != nil {
-			log.Fatalf("error retrieving disc ID: %v", err)
-		}
 		// Attempt to fetch from MusicBrainz if GNUDB fails
 		
 		discInfo, err = fetchMusicBrainzRelease(strings.Replace(mbToc, " ", "+", -1))
