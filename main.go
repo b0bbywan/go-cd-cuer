1d20611a3ab971fa058240feda17ac40a6e15de9
Mathieu Réquillart <mathieu.requillart@gmail.com>
2024-11-11 02:27:48 +0100

    modularize

--- main.go
+++ main.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"github.com/b0bbywan/go-cd-cuer/discinfo"
-	"github.com/b0bbywan/go-cd-cuer/musicbrainz"
-	"github.com/b0bbywan/go-cd-cuer/types"
-	"github.com/b0bbywan/go-cd-cuer/utils"
+	"github.com/b0bbywan/go-cd-cuer/cue"
 	"log"
 )
 
@@ -23,77 +19,10 @@ func init() {
 	flag.StringVar(&providedDiscID, "disc-id", "", "specify disc ID directly")
 }
 
-// fetchDiscInfoFromFlags returns DiscInfo, disc ID, and an error based on provided options.
-func fetchDiscInfoFromFlags() (*types.DiscInfo, string, error) {
-	// Enforce --musicbrainz with --disc-id
-	if providedDiscID != "" && musicbrainzID == "" {
-		return nil, "", fmt.Errorf("error: --disc-id option requires --musicbrainz to be set")
-	}
-
-	// If --musicbrainz is provided, fetch DiscInfo directly from MusicBrainz
-	if musicbrainzID != "" {
-		discInfo, err := musicbrainz.FetchReleaseByID(musicbrainzID)
-		if err != nil {
-			return nil, "", err
-		}
-		return discInfo, providedDiscID, nil
-	}
-	return nil, "", nil
-}
-
-func finalizeIfSuccess(discInfo *types.DiscInfo, cueFilePath string) {
-    if err := discinfo.FetchCoverArtIfNeeded(discInfo, cueFilePath); err != nil {
-        log.Printf("Error fetching cover art: %v", err)
-    }
-	// Generate the CUE file and save
-	if err := discinfo.GenerateCueFile(discInfo, cueFilePath); err != nil {
-		log.Fatalf("error: failed to generate CUE file: %v", err)
-	}
-	utils.SaveEnvFile(cueFilePath)
-	log.Printf("info: Playlist generated at %s", cueFilePath)
-}
-
 func main() {
 	flag.Parse()
 
-	if err := utils.RemoveEnvFile(); err != nil {
-		log.Fatalf("error removing env file: %v", err)
-	}
-
-	discInfo, discID, err := fetchDiscInfoFromFlags()
-	if err != nil {
-		log.Fatalf("error parsing options: %v", err)
-	}
-
-	var gnuToc string
-	if discID == "" {
-		if gnuToc, discID, err = utils.GetTocAndDiscID(); err != nil {
-			log.Fatalf("error retrieving disc ID: %v", err)
-		}
-	}
-	cueFilePath := utils.CachePlaylistPath(discID)
-
-	if utils.CheckIfPlaylistExists(cueFilePath) && !overwrite {
-		return
-	}
-
-	if discInfo != nil && discID != "" {
-		finalizeIfSuccess(discInfo, cueFilePath)
-		return
-	}
-	var mbToc string
-	if mbToc, err = utils.GetMusicBrainzDiscIDFromCmd(); err != nil {
-		log.Fatalf("error retrieving MusicBrainz disc ID: %v", err)
-	}
-
-	if err = utils.CreateFolderIfNeeded(cueFilePath); err != nil {
-		log.Fatalf("error creating folder for discID: %v", err)
-	}
-
-	// Fetch DiscInfo concurrently
-	if discInfo, err = discinfo.FetchDiscInfoConcurrently(gnuToc, mbToc); err != nil {
+	if _, err := cue.GenerateWithOptions(musicbrainzID, providedDiscID, overwrite); err != nil {
 		log.Fatalf("error: failed to generate playlist from both GNUDB and MusicBrainz: %v", err)
 	}
-
-	finalizeIfSuccess(discInfo, cueFilePath)
 }
--- utils/fileutils.go
+++ utils/fileutils.go
@@ -7,10 +7,6 @@ import (
 	"path/filepath"
 )
 
-const (
-	envFile       = "/tmp/cd_var.env"
-)
-
 var (
 	cacheLocation = fmt.Sprintf("%s/.cddb", os.Getenv("HOME"))
 )
@@ -18,19 +14,10 @@ var (
 // checkIfPlaylistExists checks if the CUE file already exists.
 func CheckIfPlaylistExists(cueFilePath string) bool {
 	if _, err := os.Stat(cueFilePath); err == nil {
-		SaveEnvFile(cueFilePath)
 		log.Printf("info: Playlist already exists at %s", cueFilePath)
 		return true
 	}
 	return false
-}
-
-// removeEnvFile removes the environment file if it exists.
-func RemoveEnvFile() error {
-	if err := os.Remove(envFile); err != nil && !os.IsNotExist(err) {
-		return fmt.Errorf("error removing env file: %v", err)
-	}
-	return nil
 }
 
 // createFolderIfNeeded creates the necessary folder for the playlist file.
@@ -45,8 +32,4 @@ func CachePlaylistPath(discID string) string {
 // Cache cover art path
 func CacheCoverArtPath(discID string) string {
 	return filepath.Join(cacheLocation, discID, "cover.jpg")
-}
-
-func SaveEnvFile(cueFile string) error {
-	return os.WriteFile(envFile, []byte(fmt.Sprintf("CUE_FILE=%s", cueFile)), 0644)
 }
